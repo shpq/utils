facf0e2c22c27a2850b41cfa03404629add9d356
shpq <denis.manichkin@mnfst.com>
2020-04-26 11:33:05 +0300

    coral implementation

--- training/utkface_age/main.py
+++ training/utkface_age/main.py
@@ -7,11 +7,10 @@ import torch.nn.functional as F
 import argparse
 import sys
 import numpy as np
-import timm
-
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+from torchvision import transforms
 from PIL import Image
 from tqdm import tqdm
 
@@ -43,8 +42,6 @@ parser.add_argument("--imp_weight", type=int, default=0)
 parser.add_argument("--batch_size", type=int, default=0)
 
 parser.add_argument("--lr", type=float, default=0)
-
-parser.add_argument("--pretrained", type=str, default=0)
 
 args = parser.parse_args()
 
@@ -167,7 +164,9 @@ class UTKFaceDataset(Dataset):
             img = augmented["image"]
 
         label = self.y[index]
-        return img, label
+        levels = [1] * label + [0] * (NUM_CLASSES - 1 - label)
+        levels = torch.tensor(levels, dtype=torch.float32)
+        return img, label, levels
 
     def __len__(self):
         return self.y.shape[0]
@@ -240,9 +239,133 @@ test_loader = DataLoader(
 ##########################
 
 
-model = nn.Sequential(timm.create_model(
-        args.pretrained, pretrained=True, num_classes=1
-    ), nn.ReLU())
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=1,
+        bias=False,
+    )
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes, grayscale):
+        self.num_classes = num_classes
+        self.inplanes = 64
+        if grayscale:
+            in_dim = 1
+        else:
+            in_dim = 3
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_dim, 64, kernel_size=7, stride=2, padding=3, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1, padding=2)
+        self.fc = nn.Linear(2048 * block.expansion, 1, bias=False)
+        self.linear_1_bias = nn.Parameter(
+            torch.zeros(self.num_classes - 1).float()
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, (2.0 / n) ** 0.5)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        logits = self.fc(x)
+        logits = logits + self.linear_1_bias
+        probas = torch.sigmoid(logits)
+        return logits, probas
+
+
+def resnet34(num_classes, grayscale):
+    """Constructs a ResNet-34 model."""
+    model = ResNet(
+        block=BasicBlock,
+        layers=[3, 4, 6, 3],
+        num_classes=num_classes,
+        grayscale=grayscale,
+    )
+    return model
 
 
 ###########################################
@@ -250,17 +373,45 @@ model = nn.Sequential(timm.create_model(
 ###########################################
 
 
+def cost_fn(logits, levels, imp):
+    val = -torch.sum(
+        (
+            F.logsigmoid(logits) * levels
+            + (F.logsigmoid(logits) - logits) * (1 - levels)
+        )
+        * imp,
+        dim=1,
+    )
+    return torch.mean(val)
+
 
 torch.manual_seed(RANDOM_SEED)
 torch.cuda.manual_seed(RANDOM_SEED)
+model = resnet34(NUM_CLASSES, GRAYSCALE)
 
 model.to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-criterion = nn.L1Loss()
+
+def compute_mae_and_mse(model, data_loader, device):
+    mae, mse, num_examples = 0, 0, 0
+    for i, (features, targets, levels) in enumerate(data_loader):
+
+        features = features.to(device)
+        targets = targets.to(device)
+
+        logits, probas = model(features)
+        predict_levels = probas > 0.5
+        predicted_labels = torch.sum(predict_levels, dim=1)
+        num_examples += targets.size(0)
+        mae += torch.sum(torch.abs(predicted_labels - targets))
+        mse += torch.sum((predicted_labels - targets) ** 2)
+    mae = mae.float() / num_examples
+    mse = mse.float() / num_examples
+    return mae, mse
+
 
 loaders = {"train": train_loader, "test": test_loader}
-datasets = {"train": train_dataset, "test": test_dataset}
 start_time = time.time()
 for epoch in range(num_epochs):
     for phase in ["train", "test"]:
@@ -272,7 +423,7 @@ for epoch in range(num_epochs):
         loader = loaders[phase]
         pbar = tqdm(enumerate(loader), total=len(loader))
 
-        for batch_idx, (features, targets) in pbar:
+        for batch_idx, (features, targets, levels) in pbar:
             scale_value = 1 / BATCH_SIZE / max(batch_idx, 1)
             pbar.set_description(
                 (
@@ -281,7 +432,7 @@ for epoch in range(num_epochs):
                         epoch + 1,
                         num_epochs,
                         batch_idx,
-                        len(datasets[phase]) // BATCH_SIZE,
+                        len(train_dataset) // BATCH_SIZE,
                         running_cost * scale_value,
                     )
                 )
@@ -289,11 +440,12 @@ for epoch in range(num_epochs):
             features = features.to(DEVICE)
             targets = targets
             targets = targets.to(DEVICE)
+            levels = levels.to(DEVICE)
             optimizer.zero_grad()
             with torch.set_grad_enabled(phase == "train"):
                 # FORWARD AND BACK PROP
-                pred = model(features)
-                cost = criterion(targets, pred)
+                logits, probas = model(features)
+                cost = cost_fn(logits, levels, imp)
 
                 if phase == "train":
                     cost.backward()
@@ -301,14 +453,35 @@ for epoch in range(num_epochs):
                     # UPDATE MODEL PARAMETERS
                     optimizer.step()
                 running_cost += cost.item() * features.size(0)
+    with torch.set_grad_enabled(False):  # save memory during inference
+
+        train_mae, train_mse = compute_mae_and_mse(
+            model, train_loader, device=DEVICE
+        )
+        test_mae, test_mse = compute_mae_and_mse(
+            model, test_loader, device=DEVICE)
+
+        s = "MAE/RMSE: | Train: %.2f/%.2f | Test: %.2f/%.2f" % (
+            train_mae,
+            torch.sqrt(train_mse),
+            test_mae,
+            torch.sqrt(test_mse),
+        )
+
+        print(s)
+
     all_pred = []
+    all_probas = []
     with torch.set_grad_enabled(False):
-        for batch_idx, (features, targets) in enumerate(test_loader):
+        for batch_idx, (features, targets, levels) in enumerate(test_loader):
 
             features = features.to(DEVICE)
-            preds = model(features)
-            all_pred += [str(int(x)) for x in preds.detach().cpu().numpy()]
-
+            logits, probas = model(features)
+            all_probas.append(probas)
+            predict_levels = probas > 0.5
+            predicted_labels = torch.sum(predict_levels, dim=1)
+            lst = [str(int(i)) for i in predicted_labels]
+            all_pred.extend(lst)
 
     with open(TEST_PREDICTIONS, "w") as f:
         all_pred = ",".join(all_pred)
