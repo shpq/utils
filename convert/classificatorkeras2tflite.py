2d8edb8fb4192db185d86c2da17804291d772c78
shpq <denis.manichkin@mnfst.com>
2020-09-28 12:10:36 +0300

    fix keras2smth convertion

--- convert/classificatorkeras2tflite.py
+++ convert/classificatorkeras2tflite.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 from tensorflow.compat.v1.lite import TFLiteConverter
 from tensorflow.keras.models import load_model
 from utils import url2image, default_transformation
+import tensorflow_model_optimization as tfmot
+from tensorflow import keras
+import argparse
+from pprint import pprint
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
@@ -15,7 +19,13 @@ if __name__ == "__main__":
                       help="Path to .txt file with urls to compare models")
 
     args = args.parse_args()
-    converter = TFLiteConverter.from_keras_model_file(args.keras_file)
+
+    with tfmot.quantization.keras.quantize_scope():
+        keras_model = keras.models.load_model(args.keras_file)
+
+    keras_model.summary()
+    converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+
     if args.quantized:
         converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
@@ -25,9 +35,10 @@ if __name__ == "__main__":
         f.write(tflite_model)
 
     if args.test_path:
-        keras_model = load_model(args.keras_file)
+
         tflite_model = tf.lite.Interpreter(model_path=args.save_path)
         tflite_model.allocate_tensors()
+
         input_details = tflite_model.get_input_details()
         output_details = tflite_model.get_output_details()
         input_shape = input_details[0]["shape"]
@@ -37,17 +48,18 @@ if __name__ == "__main__":
         d = []
         for url in urls:
             image = url2image(url, (input_shape[1], input_shape[2]))
-            transformed_image = trans(np.array(image))
+            transformed_image = trans(image=np.array(image))["image"]
+            transformed_image = np.expand_dims(transformed_image, axis=0)
             # maybe wrapped?
-            keras_pred = keras_model.predict(
-                np.expand_dims(transformed_image, axis=0))
-            tflite_model.set_tensor(input_details[0]["index"], transformed_image)
+            keras_pred = keras_model.predict(transformed_image)
+            tflite_model.set_tensor(
+                input_details[0]["index"], transformed_image)
             tflite_model.invoke()
             tflite_pred = tflite_model.get_tensor(output_details[0]["index"])
 
             d.append({
                 "url": url,
                 "keras": keras_pred,
-                "tflite": tflite_predm
+                "tflite": tflite_pred
             })
         pprint(d)
